e7fd9de5d04c3547629d93e6be934cb9548e177c
Raven <raven@Ravens-MacBook-Air.local>
2020-12-18 10:39:26 +0800

    finish structured-english

---
+++ final.py
@@ -0,0 +1,16 @@
+"""
+The program is trying to display the number of grades, the average grade, 
+and the percentage of grades that are above the average grade in a final exam.
+We first read in text file final.txt
+
+Once the text file has been read into memory, we close the file.
+The grades in text file will be imported to the program.
+The program will read the input text file to assign the grades to a list of integer.
+
+We then define a function that calculate the number of grades listed in the text file.
+then, calculate the average grade which take the total grade(sum) / number of grades
+then, we define a function that calculate the percentage of grades that are above the average grade (counter/length)
+we format the output to float and add percentage
+
+Print out the calculated values to the assigned variables.
+"""
